2a4b70ae91421f6788d4519a97b2db0dd25f4a33
Xframex <loukili.issmael@.com>
2024-04-04 16:06:37 +0000

    task 2

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" LIFO caching
+"""
+from base_caching import BaseCaching
+
+class LIFOCache(BaseCaching):
+    """ LIFO cache class for discaed the last element of item
+    """
+    def __init__(self):
+        """ Constructor method
+        """
+        super().__init__()
+        self.cache_data = {}
+    
+    def put(self, key, item):
+        """ Add a item in the cache 
+        """
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                last = list(self.cache_data.keys())[-1]
+                self.cache_data.pop(last)
+                print("DISCARD: {}".format(last))
+            self.cache_data[key] = item
+        
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
