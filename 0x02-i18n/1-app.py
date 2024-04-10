59d756e6f8eba1a8435a6991e7fb0ab88fbf06aa
Xframex <loukili.issmael@.com>
2024-04-10 02:36:20 +0000

    task 2

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python3
-
-from flask import Flask, render_template
+"""Flask app.
+"""
 from flask_babel import Babel
+from flask import Flask, render_template
+
 
 class Config:
-    """Config class.
+    """Flask Babel configuration.
     """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-    app = Flask(__name__)
-    app.url_map.strict_slashes = False
-    app.config.from_object(Config)
-    babel = Babel(app)
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-    @app.route('/')
-    def get_index() -> str:
-        """revoke index html.
-        """
-        return render_template('0-index.html')
-    
-    if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@app.route('/')
+def get_index() -> str:
+    """revok index.
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
