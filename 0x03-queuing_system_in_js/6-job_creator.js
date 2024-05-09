502b84f0ee9e49a567e00c84e4dbe89ed84e2848
Xframex <loukili.issmael@gmail.com>
2024-05-08 18:40:33 -0700

    create a job with kue

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,21 @@
+#!/usr/bin/yarn dev
+import { createQueue } from "kue";
+
+const queue = createQueue({name: 'push_notification_code'});
+
+const jobData = queue.create('push_notification_code', {
+    phoneNumber: '4153518780',
+    message: 'code for verify your account',
+    });
+
+jobData.on('enqueue', () => {
+    console.log(`Notification job created: ${jobData.id}`);
+});
+
+jobData.on('complete', () => {
+    console.log('Notification job completed');
+});
+
+jobData.save((err) => {
+    if (!err) console.log(`Notification job created: ${jobData.id}`);
+});
